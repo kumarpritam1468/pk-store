41ce89b6028019c0946479d645a2ee9b63bb7cec
Pritam <manoharipritam@gmail.com>
2024-09-25 21:02:46 +0530

    complete navbar with cartModal

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
+import CartModal from "./CartModal"
 
 const NavIcons = () => {
     const router = useRouter();
@@ -24,7 +25,7 @@ const NavIcons = () => {
         <div className=" flex items-center justify-center gap-4 xl:gap-6 px-4 relative">
             <Image src="/profile.png" height={24} width={24} alt="profile" className=" cursor-pointer" onClick={handleProfile} />
 
-            {profileOpen && 
+            {profileOpen &&
                 <div className=" absolute flex gap-2 flex-col p-4 left-0 top-12 rounded-lg shadow-[0_3px_10px_rgba(0,0,0,0.2)] z-40">
                     <Link href="/profile">Profile</Link>
                     <Link href="/logout">Log out</Link>
@@ -32,7 +33,15 @@ const NavIcons = () => {
             }
 
             <Image src="/notification.png" height={24} width={24} alt="notification" className=" cursor-pointer" />
-            <Image src="/cart.png" height={24} width={24} alt="cart" className=" cursor-pointer" onClick={() => setCartOpen(!cartOpen)} />
+
+            <div className=" relative cursor-pointer">
+                <Image src="/cart.png" height={24} width={24} alt="cart" onClick={() => setCartOpen(!cartOpen)} />
+                <div className=" absolute bg-main-red rounded-full size-5 -top-2 -right-2 text-sm flex items-center justify-center text-white">2</div>
+            </div>
+
+            {cartOpen &&
+                <CartModal />
+            }
         </div>
     )
 }
